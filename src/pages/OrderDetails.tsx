8cc0eeb423661f16764c628ef2639bb305e6ade5
saeed safari <saeid.elitem@gmail.com>
2024-05-16 13:54:40 +0330

    Implementation OrderDetails page and handle add, remove and place order

--- src/pages/OrderDetails.tsx
+++ src/pages/OrderDetails.tsx
@@ -10,6 +10,7 @@ import {
 } from "../store/slices/orderSlice";
 import { Grid, IconButton, Box } from "@mui/material";
 import { getProducts, selectProducts } from "../store/slices/productSlice";
+import ProductList from "../components/product/ProductList";
 import {
   getCustomerFromState,
   getCustomers,
@@ -18,6 +19,9 @@ import {
 } from "../store/slices/customerSlice";
 import { useNavigate } from "react-router-dom";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import CustomerInfo from "../components/customer/CustomerInfo";
+import OrderDetailsAction from "../components/order/OrderDetailsAction";
+import OrderDetailsList from "../components/order/OrderDetailsList";
 import useLocalStorage from "../hooks/useLocalStorage";
 import { AppThunkDispatch } from "../store";
 import {
@@ -28,7 +32,7 @@ import {
   IProduct,
 } from "../models/index.model";
 import SectionTitle from "../common/SectionTitle";
-import CustomerInfo from "../components/customer/CustomerInfo";
+import PaperWrapper from "../common/PaperWrapper";
 
 interface StoredOrder {
   id: string;
@@ -57,6 +61,64 @@ const OrderDetails: React.FC = () => {
     }
   }, [order, customers, dispatch]);
 
+  const onAddPruduct = useCallback(
+    (product: IProduct) => {
+      dispatch(addItemToOrder(product));
+    },
+    [dispatch]
+  );
+
+  const onRemoveProduct = useCallback(
+    (product: IOrderProductItem) => {
+      dispatch(removeItemFromOrder(product));
+    },
+    [dispatch]
+  );
+
+  const orderItems: IOrderProductItem[] | undefined = useMemo(
+    () =>
+      order?.items?.map((item: IOrderItem) => {
+        const product: IProduct | undefined = products.find(
+          (product: IProduct) => product.id === item["product-id"]
+        );
+        return {
+          ...item,
+          product,
+        };
+      }),
+    [order, products]
+  );
+
+  const handlePlaceOrder = useCallback(() => {
+    const storedOrders = value;
+    const findedOrder = storedOrders.find(
+      (item: IOrder) => item?.id === order?.id
+    );
+
+    if (!findedOrder) {
+      dispatch(placeOrder());
+      navigate("/order");
+      return;
+    }
+
+    const itemsLengthChanged =
+      order?.items?.length !== findedOrder.items.length;
+
+    const quantityChanged = order?.items?.some(
+      (item: IOrderItem, index: number) => {
+        const prevItem: IOrderItem = findedOrder.items[index];
+        return !prevItem || item?.quantity !== prevItem?.quantity;
+      }
+    );
+
+    const orderItemsChanged = itemsLengthChanged || quantityChanged;
+
+    if (orderItemsChanged) {
+      dispatch(placeOrder());
+      navigate("/order");
+    }
+  }, [dispatch, order, navigate, value]);
+
   return (
     <Box p={5}>
       <IconButton edge="end" onClick={() => navigate(-1)}>
@@ -64,11 +126,29 @@ const OrderDetails: React.FC = () => {
       </IconButton>
 
       <SectionTitle title="Order Details" align="center" variant="h5" mb={3} />
-      <Grid item xs={12} md={6}>
-        <CustomerInfo customer={customer} />
-      </Grid>
 
-      <Grid container spacing={2}></Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          <CustomerInfo customer={customer} />
+        </Grid>
+
+        <Grid item xs={12} md={6}>
+          <OrderDetailsAction order={order} onPlace={handlePlaceOrder} />
+        </Grid>
+
+        <Grid item xs={12} md={6}>
+          <OrderDetailsList
+            orderItems={orderItems}
+            onRemove={onRemoveProduct}
+          />
+        </Grid>
+
+        <Grid item xs={12} md={6}>
+          <PaperWrapper>
+            <ProductList products={products} onAddProduct={onAddPruduct} />
+          </PaperWrapper>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
