233c60e9d9f2b49dc664a9709337df25c92a9373
saeed safari <saeid.elitem@gmail.com>
2024-05-16 13:25:27 +0330

    Implementation useLocalStorage hook to handle get and set date from localStorage

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+
+function useLocalStorage<T>(
+  key: string,
+  initialValue: T
+): [T, React.Dispatch<React.SetStateAction<T>>] {
+  const [value, setValue] = useState<T>(() => {
+    const storedValue = localStorage.getItem(key);
+    return storedValue !== null ? JSON.parse(storedValue) : initialValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+}
+
+export default useLocalStorage;
